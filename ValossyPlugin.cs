0da6b23f955bed7a1d618ae8f039397838a40fd7
Sasa <sasa.seles@valosstudio.com>
2023-12-25 13:39:59 +0900

    Editor Plugin init

---
+++ ValossyPlugin.cs
@@ -0,0 +1,17 @@
+#if TOOLS
+using Godot;
+
+namespace Valossy;
+
+[Tool]
+public partial class ValossyPlugin : EditorPlugin
+{
+    public override void _EnterTree()
+    {
+    }
+
+    public override void _ExitTree()
+    {
+    }
+}
+#endif
