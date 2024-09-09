355c1c037c568c622922a2b98ffb2101e62f62de
Sasa <sasa.seles@valosstudio.com>
2024-09-09 20:14:40 +0900

    Added ProgressBarBinding

---
+++ Bindings/BindingTypes/Types/ProgressBarBinding.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Godot;
+
+namespace Valossy.Bindings.BindingTypes.Types;
+
+public class ProgressBarBinding : IBindingTypeHandler
+{
+    private readonly Dictionary<string, PropertyChangedEventHandler> delegates;
+
+    public ProgressBarBinding()
+    {
+        delegates = new Dictionary<string, PropertyChangedEventHandler>();
+    }
+
+    public void Bind(object modelObject, string modelPropertyName, object viewObject, BindingMode bindingMode)
+    {
+        if (viewObject is not ProgressBar control)
+        {
+            return;
+        }
+
+        if (modelObject is INotifyPropertyChanged notifyPropertyChanged)
+        {
+            string uniqueName = CreateUniqueName(modelObject, modelPropertyName, control);
+
+            if (delegates.ContainsKey(uniqueName) == true)
+            {
+                //Already subscribed
+
+                return;
+            }
+
+            PropertyChangedEventHandler handler = ChangedOnPropertyChanged;
+
+            // Set the initial value
+            this.SetValue(modelObject, modelPropertyName, control);
+
+            delegates.Add(uniqueName, handler);
+
+            notifyPropertyChanged.PropertyChanged += handler;
+        }
+
+        void ChangedOnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (Equals(e.PropertyName, modelPropertyName))
+            {
+                this.SetValue(modelObject, modelPropertyName, control);
+            }
+        }
+    }
+
+    public void UnBind(object modelObject, string modelPropertyName, object viewObject)
+    {
+        if (viewObject is not Label control)
+        {
+            return;
+        }
+
+        string uniqueName = CreateUniqueName(modelObject, modelPropertyName, control);
+
+        bool exists = this.delegates.TryGetValue(uniqueName, out PropertyChangedEventHandler handler);
+
+        if (exists == false)
+        {
+            //Nothing to unsubscribe
+
+            return;
+        }
+
+        if (modelObject is INotifyPropertyChanged notifyPropertyChanged)
+        {
+            notifyPropertyChanged.PropertyChanged -= handler;
+        }
+    }
+
+    public Type ProcessingType()
+    {
+        return typeof(ProgressBar);
+    }
+
+    private void SetValue(object modelObject, string modelPropertyName, ProgressBar control)
+    {
+        object value = modelObject.GetType().GetProperty(modelPropertyName).GetValue(modelObject);
+
+        control.Value = Convert.ToDouble(value);;
+    }
+
+    private string CreateUniqueName(object modelObject, string modelPropertyName, Control viewObject)
+    {
+        return $"{modelObject.GetHashCode()}{modelPropertyName}{viewObject.GetInstanceId()}";
+    }
+}
