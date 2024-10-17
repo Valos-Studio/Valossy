20baea4ee622b385254e22ff642b7f02421930a4
Sasa <sasa.seles@valosstudio.com>
2024-10-17 19:54:51 +0900

    Circle check update

--- Helpers/Areas/CircleCheck.cs
+++ Helpers/Areas/CircleCheck.cs
@@ -4,7 +4,12 @@ namespace Valossy.Helpers.Areas;
 
 public static class CircleCheck
 {
-    public static bool IsInside(Vector2 rectanglePosition, Vector2 rectangleSize, Vector2 circleCenter, float circleRadius)
+    /// <summary>
+    /// This works for rectangles that have their starting point on the left upper corner
+    /// </summary>
+    /// <returns>Returns true when the rectangle is inside of a circle</returns>
+    public static bool IsInside(Vector2 rectanglePosition, Vector2 rectangleSize, Vector2 circleCenter,
+        float circleRadius)
     {
         Vector2[] innerCorners = new Vector2[4]
         {
@@ -16,6 +21,38 @@ public static class CircleCheck
             new Vector2(rectanglePosition.X, rectanglePosition.Y + rectangleSize.Y),
             // Bottom-right
             new Vector2(rectanglePosition.X + rectangleSize.X, rectanglePosition.Y + rectangleSize.Y)
+        };
+
+        foreach (Vector2 corner in innerCorners)
+        {
+            if (IsPointInsideCircle(corner, circleCenter, circleRadius) == false)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// This works for rectangles that have their starting point centered in the middle
+    /// </summary>
+    /// <returns>Returns true when the rectangle is inside of a circle</returns>
+    public static bool IsInsideCentered(Vector2 rectanglePosition, Vector2 rectangleSize, Vector2 circleCenter,
+        float circleRadius)
+    {
+        rectangleSize /= 2;
+
+        Vector2[] innerCorners = new Vector2[4]
+        {
+            // Top-left
+            new Vector2(rectanglePosition.X - rectangleSize.X, rectanglePosition.Y + rectangleSize.Y),
+            // Top-right
+            new Vector2(rectanglePosition.X + rectangleSize.X, rectanglePosition.Y + rectangleSize.Y),
+            // Bottom-left
+            new Vector2(rectanglePosition.X - rectangleSize.X, rectanglePosition.Y - rectangleSize.Y),
+            // Bottom-right
+            new Vector2(rectanglePosition.X + rectangleSize.X, rectanglePosition.Y - rectangleSize.Y)
         };
 
         foreach (Vector2 corner in innerCorners)
