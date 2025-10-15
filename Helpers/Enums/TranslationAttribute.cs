26ab40e4f8bafafa382293bd0721c9d5ebd8695a
Sasa <sasa.seles@valosstudio.com>
2025-10-16 05:06:14 +0900

    Adds enum translation extension
    Introduces an extension to retrieve translations for enum values
    using a custom TranslationAttribute.
    
    This allows to define a human-readable string associated with each
    enum value, which can be used for localization or display purposes.

---
+++ Helpers/Enums/TranslationAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Valossy.Helpers.Enums;
+
+[AttributeUsage(AttributeTargets.All, Inherited = true, AllowMultiple = false)]
+public sealed class TranslationAttribute : Attribute
+{
+    public string Translation { get; }
+
+    public TranslationAttribute(string translation)
+    {
+        Translation = translation;
+    }
+}
